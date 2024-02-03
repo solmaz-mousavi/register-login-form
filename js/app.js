48daa1ab384ee011e622cc42b887c624f7fcf452
Solmaz Mousavi <153442811+solmaz-mousavi@users.noreply.github.com>
2024-02-03 12:54:54 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
 const registerTitle = document.getElementById('register-title')
 const loginTitle = document.getElementById('login-title')
 const loginForm = document.getElementById('login-container')
-console.log(registerTitle)
-console.log(loginTitle)
-console.log(loginForm)
-
 
 const loginFormHandler = () => loginForm.style.top = '100px'
 const registerFormHandler = () => loginForm.style.top = '380px'
 
-
-
 loginTitle.addEventListener('click', loginFormHandler)
 registerTitle.addEventListener('click', registerFormHandler)
-
-
